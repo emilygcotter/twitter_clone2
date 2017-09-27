92742f0b3529285a78760d6641c300a2269c10d2
EMILYCOTTER <EMILY.G.COTTER@GMAIL.COM>
2017-09-27 19:42:55 -0400

    added follow user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def follow
+    user = User.find(params[:following_id])
+    Relationship.create(follower_id: current_user.id, following_id: params[:following_id])
+    redirect_to user, notice: 'You are now following this user.'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
 	get 'tags/:id' => 'tweets#show_tagged_tweets'
 
-	get 'users/:id/show' => 'users#show', as: 'show_user'
+	get 'users/:id/show' => 'users#show', as: 'user'
+
+	post 'users/follow' => 'users#follow', as: 'follow_user'
 
 	devise_for :users
 	root 'tweets#index'
 
 	resources :tweets
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
